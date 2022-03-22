fafa003a0d2b117ffcd551de6105ad76bb06e15e
LucaVanossi <vanossi.luca13@gmail.com>
2022-03-22 15:43:07 +0100

    Work in progress

---
+++ MyScript.js
@@ -0,0 +1,9 @@
+function calculate(opreator,res) {
+    var km,età;
+    a = document.getElementById("km").value;
+    b = document.getElementById("età").value;
+    var calculate = eval(a*opreator*b);
+    document.getElementById("Risultato").value = result;
+    return Risultato;
+    alert("Risultato");
+  }      
