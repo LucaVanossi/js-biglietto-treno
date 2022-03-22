855161f3e488dbfed0f97441d921c0365e4a955a
LucaVanossi <vanossi.luca13@gmail.com>
2022-03-22 17:00:43 +0100

    Lavoro Finito

--- MyScript.js
+++ MyScript.js
@@ -1,9 +1,14 @@
-function calculate(opreator,res) {
-    var km,età;
-    a = document.getElementById("km").value;
-    b = document.getElementById("età").value;
-    var calculate = eval(a*opreator*b);
-    document.getElementById("Risultato").value = result;
-    return Risultato;
-    alert("Risultato");
-  }      
+  function calculate(){
+    var km,age,costokm,prezzototale,sconto20,sconto40,risultato;
+    km = document.getElementById("km").value;
+    age = document.getElementById("age").value;
+    prezzototale=km*0.21;
+    sconto20=prezzototale*0.8;
+    sconto40=prezzototale*0.6;
+    if (age <18) {risultato=prezzototale-sconto20}
+    else if (age>65) {risultato=prezzototale-sconto40}
+    else {risultato=prezzototale}
+    risultato=risultato.toFixed(2);  
+    document.getElementById("Calcolo").value = risultato;
+} 
+
